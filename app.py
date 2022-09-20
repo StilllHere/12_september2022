747d3a3458f1b2fcef13be4f2c1ebf91efe90e97
StilllHere <106540985+StilllHere@users.noreply.github.com>
2022-09-20 19:51:35 +0300

    do it

--- app.py
+++ app.py
@@ -1,36 +1,23 @@
-from flask import Flask, request, render_template, send_from_directory
-# from functions import ...
+from flask import Flask, send_from_directory
+from main.view import main_blueprint
+from loader.view import loader_blueprint
+import logging
 
 POST_PATH = "posts.json"
 UPLOAD_FOLDER = "uploads/images"
 
 app = Flask(__name__)
+app.register_blueprint(main_blueprint)
+app.register_blueprint(loader_blueprint)
 
-
-@app.route("/")
-def page_index():
-    pass
-
-
-@app.route("/list")
-def page_tag():
-    pass
-
-
-@app.route("/post", methods=["GET", "POST"])
-def page_post_form():
-    pass
-
-
-@app.route("/post", methods=["POST"])
-def page_post_upload():
-    pass
-
+logging.basicConfig(level=logging.INFO, handlers=[logging.FileHandler(filename="basic.log", encoding='utf-8', mode='a+')])
 
 @app.route("/uploads/<path:path>")
 def static_dir(path):
     return send_from_directory("uploads", path)
 
 
-app.run()
+if __name__ == "__main__":
+    app.run()
+
 
--- functions.py
+++ functions.py
@@ -1,1 +1,39 @@
 import json
+from flask import request
+
+def read_file():
+    """
+    Чтение из файла
+    """
+    with open('posts.json', 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+
+def get_posts_by_word(word):
+    """
+    Поиск постов
+    """
+    result =[]
+    for post in read_file():
+        if word.lower() in post['content'].lower():
+           result.append(post)
+    return result
+
+def safe_pic(picture):
+    """
+    Сохранение картинок
+    """
+    picture = request.files.get("picture")
+    filename = picture.filename
+    picture.save(f"./uploads/images/{filename}")
+    return f"./uploads/images/{filename}"
+
+def post_text_add(post):
+    """
+    Сохранение постов
+    """
+    posts = read_file()
+    posts.append(post)
+    with open('posts.json','w', encoding='utf-8') as f:
+        json.dump(posts, f)
+    return posts
